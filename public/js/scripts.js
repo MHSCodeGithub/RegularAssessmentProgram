669fbce15425bb7b389d5c2b5e56e1fc0e800495
voxace <dave@voxace.com>
2018-05-28 13:30:10 +1000

    more ie fixing

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,25 @@
+function getInternetExplorerVersion()
+{
+  var rv = -1;
+  if (navigator.appName == 'Microsoft Internet Explorer')
+  {
+    var ua = navigator.userAgent;
+    var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null)
+      rv = parseFloat( RegExp.$1 );
+  }
+  else if (navigator.appName == 'Netscape')
+  {
+    var ua = navigator.userAgent;
+    var re  = new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null)
+      rv = parseFloat( RegExp.$1 );
+  }
+  return rv;
+}
+
+console.log('IE version:', getInternetExplorerVersion());
+
 $(document).ready(function() {
 
 
