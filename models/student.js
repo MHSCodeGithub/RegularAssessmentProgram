794bf8ba2295c4cd88ca30dc1bca04c985aaf7fc
voxace <dave@voxace.com>
2018-11-09 23:42:21 +1100

    Update models/student.js

--- models/student.js
+++ models/student.js
@@ -2,16 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create schemas
-const ScoreSchema = new Schema({
-  subject: String,
-  code: String,
-  teacher: String,
-  value: {
-    type: Number,
-    min: [0, "Too low"],
-    max: [5, "Too high"]
-  }
-});
+const ScoreSchema = new Schema(
+  {
+    subject: String,
+    code: String,
+    teacher: String,
+    value: {
+      type: Number,
+      min: [0, "Too low"],
+      max: [5, "Too high"]
+    }
+  },
+  { versionKey: false }
+);
 
 const RapSchema = new Schema(
   {
@@ -31,21 +34,24 @@ const RapSchema = new Schema(
   { versionKey: false }
 );
 
-const StudentSchema = new Schema({
-  name: String,
-  username: {
-    type: String,
-    lowercase: true
+const StudentSchema = new Schema(
+  {
+    name: String,
+    username: {
+      type: String,
+      lowercase: true
+    },
+    gender: String,
+    id: {
+      type: Number,
+      index: { unique: true }
+    },
+    access: Number,
+    longTermAverage: Number,
+    rap: [RapSchema]
   },
-  gender: String,
-  id: {
-    type: Number,
-    index: { unique: true }
-  },
-  access: Number,
-  longTermAverage: Number,
-  rap: [RapSchema]
-});
+  { versionKey: false }
+);
 
 // Create model
 const Student = mongoose.model("student", StudentSchema);
